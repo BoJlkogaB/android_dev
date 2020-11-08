72c23aa8f0203c4438e327e7879b5b858d37821c
bojlk <bojlkogab.03@gmail.com>
2020-11-08 19:15:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/perfectweather/ui/cities/CitiesViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.perfectweather.ui.cities
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CitiesViewModel : ViewModel() {
+
+
+}
---
+++ app/src/main/java/com/example/perfectweather/ui/weather/WeatherViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.perfectweather.ui.weather
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class WeatherViewModel : ViewModel() {
+
+}
